c83de3ad87d322a8d22b0669ccde53e05fd67b22
NikolajKorolev <nikki2.0@mail.ru>
2019-01-13 16:21:17 +0300

    14

---
+++ ConsoleApp/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Car
+    {
+        public void start()
+        {
+
+        }
+
+        public void stop()
+        {
+
+        }
+
+        public int drive(int howlong)
+        {
+            int distance = howlong * 60;
+            return distance;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            Console.WriteLine(car.drive(5));
+
+        }
+    }
+}
